4d00c98d20626350539c5009bcf54bcaadd86250
Hunter Parcells <hunterparcells@gmail.com>
2024-02-13 23:42:23 -0500

    feat: Reorder ascii and title.

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function formatSolution(solution) {
 (async () => {
   const response = await fetch('https://www.osudle.com/api/dailies/');
   const data = await response.json();
+
   const solution = data[data.length - 1];
-
-  console.log(formatSolution(solution));
-
   const ascii = await asciify(solution.background, {
     fit: 'width',
     width: process.stdout.columns * 0.5,
   });
+
   console.log(ascii);
+  console.log(formatSolution(solution));
 })();
